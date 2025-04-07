e626307e941a9f3ba3acd34257d3c048ff064111
Diego Torres <diego.torres@gmail.com>
2025-04-08 01:54:34 +0200

    Include a basic autoloader function to test the library

---
+++ autoloader.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * Autoload function registration:
+ * Registers a function to automatically load classes when they are not already included.
+ * This code converts class names with namespaces into file paths to automatically load them.
+ */
+spl_autoload_register(function ($classname) {
+
+    // Removes the leading backslash (\) if it exists.
+    // This is useful if the class is called with an absolute name (e.g., "\Namespace\ClassName").
+    $classname = ltrim($classname, "\\");
+
+    /*
+     * Uses regular expressions to separate the namespace part and the class name.
+     * - The pattern '/^(.+)?([^\\\\]+)$/U' captures everything before the class name (if any) and the class name itself.
+     * - The captured parts are stored in the `$match` array.
+     */
+    preg_match('/^(.+)?([^\\\\]+)$/U', $classname, $match);
+
+    // Replaces backslashes (\) in the namespace with slashes (/) to create file paths.
+    // It also replaces underscores (_) with slashes to ensure compatibility with file paths.
+    $classname = str_replace("\\", "/", $match[1])
+        . str_replace(["\\", "_"], "/", $match[2])
+        . ".php"; // Appends the ".php" extension at the end.
+
+    /*
+     * Finally, includes the corresponding file that contains the class.
+     * It looks in the "src" directory and builds the final path using `DIRECTORY_SEPARATOR` for better compatibility across operating systems.
+     */
+    include_once "src" . DIRECTORY_SEPARATOR . $classname;
+});
